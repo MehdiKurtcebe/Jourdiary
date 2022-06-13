a9d27ac504871b3cf9e3c47c2fc34ea31b8dfd76
ErselEren <73073947+ErselEren@users.noreply.github.com>
2022-06-13 22:09:34 +0300

    Update UI.java

--- src/UI.java
+++ src/UI.java
@@ -247,15 +247,19 @@ public class UI {
 		System.out.println("\n0 - Exit Program");
 		System.out.println("1 - Login");
 		System.out.println("2 - Sign up");
+		System.out.printf("===================");
 	}
 
 	private static void home_screen_menu(){
+		System.out.printf("\n\n\n===================");
 		System.out.println("0 - Exit Program");
 		System.out.println("1 - Profile ");
 		System.out.println("2 - Sign out ");
+		System.out.printf("===================");
 	}
 
 	private static void user_profile_menu(){
+		System.out.println("\n\n===========================");
 		System.out.println("0 - Back");
 		System.out.println("1 - View All Jourdiary");
 		System.out.println("2 - Daily Section");
@@ -265,31 +269,39 @@ public class UI {
 		System.out.println("6 - Exercise Plan");
 		System.out.println("7 - Food Menu");
 		System.out.println("8 - Hobbies ");
+		System.out.printf("==========================");
 	}
 
 	private static void task_menu(){
+		System.out.println("\n\n================================");
 		System.out.println("0 - Back");
 		System.out.println("1 - Display Tasks of the Days");
 		System.out.println("2 - Add Task");
 		System.out.println("3 - Edit Task");
 		System.out.println("4 - Mark Task As Completed");
+		System.out.printf("==================================");
 	}
 
 	private static void hobbies_menu(){
+		System.out.println("\n\n===========================");
 		System.out.println("0 - Back");
 		System.out.println("1 - Display All Hobbies");
 		System.out.println("2 - Add Hobbies");
 		System.out.println("3 - Edit Hobbies");
+		System.out.printf("============================");
 	}
 
 	private static void daily_section_menu(){
+		System.out.println("\n\n=================================");
 		System.out.println("0 - Back");
 		System.out.println("1 - Display All Daily Sections");
 		System.out.println("2 - Add Daily Section");
 		System.out.println("3 - Edit Daily Section");
+		System.out.printf("===================================");
 	}
 
 	private static void to_do_menu(){
+		System.out.println("\n\n====================================");
 		System.out.println("0 - Back");
 		System.out.println("1 - Display Current To-Dos");
 		System.out.println("2 - Display Completed To-Dos");
@@ -297,21 +309,26 @@ public class UI {
 		System.out.println("4 - Edit To-Do List");
 		System.out.println("5 - Mark a To-Do List as Completed");
 		System.out.println("6 - Mark a Task as Completed");
+		System.out.printf("=======================================");
 	}
 
 	private static void diet_plan_menu(){
+		System.out.println("\n\n=============================================");
 		System.out.println("0 - Back");
 		System.out.println("1 - Display All Diet Plans");
 		System.out.println("2 - Create Diet Plan");
 		System.out.println("3 - Edit Diet Plan");
 		System.out.println("4 - Filter Foods in Diet Plan by Calorie");
+		System.out.printf("===============================================");
 	}
 
 	private static void exercise_plan_menu(){
+		System.out.println("\n\n===========================================");
 		System.out.println("0 - Back");
 		System.out.println("1 - Display All Exercise Plans");
 		System.out.println("2 - Display Exercise Plan of the Day");
 		System.out.println("3 - Add Exercise Plan");
+		System.out.printf("==============================================");
 	}
 
 }
