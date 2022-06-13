080dda31ed806a9150255895b4786f7de2777ea9
ErselEren <73073947+ErselEren@users.noreply.github.com>
2022-06-13 15:18:19 +0300

    Update Recipes.java

--- src/Recipes.java
+++ src/Recipes.java
@@ -1,3 +1,9 @@
-public class Recipes{
+public class Recipes<String>  implements Comparable<Recipes<String>> {
 
+    @Override
+    public int compareTo(Recipes<String> o) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
 }
