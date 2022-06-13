9e8b0faea1c3f3d71784cb883c087ba4f6e54570
ErselEren <73073947+ErselEren@users.noreply.github.com>
2022-06-13 16:13:11 +0300

    Update DietPlan.java

--- src/DietPlan.java
+++ src/DietPlan.java
@@ -2,17 +2,17 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class DietPlan {
-	private Queue<FoodMenu> dailyMenus;
+	private Queue<Food> dailyMenus;
 
 	public DietPlan() {
 		dailyMenus = new LinkedList<>();
 	}
 
-	public Queue<FoodMenu> getDailyMenus() {
+	public Queue<Food> getDailyMenus() {
 		return dailyMenus;
 	}
 
-	public void setDailyMenus(Queue<FoodMenu> dailyMenus) {
+	public void setDailyMenus(Queue<Food> dailyMenus) {
 		this.dailyMenus = dailyMenus;
 	}
 }
