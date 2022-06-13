37247b517760b252a909fc4881ce3e0dc47216fc
ErselEren <73073947+ErselEren@users.noreply.github.com>
2022-06-13 15:19:26 +0300

    Delete Recipes.java

--- src/Recipes.java
+++
@@ -1,9 +0,0 @@
-public class Recipes<String>  implements Comparable<Recipes<String>> {
-
-    @Override
-    public int compareTo(Recipes<String> o) {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-    
-}
