0903b94aeeda3802ebb537344d1d5210ed6684bf
ErselEren <73073947+ErselEren@users.noreply.github.com>
2022-06-13 22:09:07 +0300

    Update DietPlan.java

--- src/DietPlan.java
+++ src/DietPlan.java
@@ -1,18 +1,38 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Iterator;
+import java.util.NavigableSet;
+import java.util.TreeSet;
 
-public class DietPlan {
-	private Queue<Food> dailyMenus;
+public class DietPlan implements Comparable<DietPlan>{
+	private NavigableSet<Food> Menu = new TreeSet<Food>();
 
 	public DietPlan() {
-		dailyMenus = new LinkedList<>();
+		Menu = new TreeSet<Food>();
 	}
 
-	public Queue<Food> getDailyMenus() {
-		return dailyMenus;
+	public void setDailyMenus(NavigableSet<Food> dailyMenus) {
+		this.Menu = dailyMenus;
 	}
 
-	public void setDailyMenus(Queue<Food> dailyMenus) {
-		this.dailyMenus = dailyMenus;
+	public void print_DietPlan(){
+
 	}
+
+	public void add(Food food){
+		Menu.add(food);
+	}
+
+	public Iterator<Food> getIterator(){
+		return Menu.iterator();
+	}
+
+	public NavigableSet<Food> getMenu(){
+		return Menu;
+	}
+
+	@Override
+	public int compareTo(DietPlan o) {
+		System.out.printf("\n DIET PLAN CLASS COMPARETO");
+		return 0;
+	}
+	
 }
